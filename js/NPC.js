b944a95a815eb35e42e1d9505ddee6531a1d1014
Gabriel <jcfgabriel@gmail.com>
2016-09-20 21:01:27 -0300

    sprites and new class

---
+++ js/NPC.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Gabriel on 20/09/2016.
+ */
+/*
+    @id
+    @name
+    @sprite
+    @pokemonList
+    @itens
+ */
+var NPC = function(name, sprite, pokemonList, itens){
+    this.name = name;
+    this.sprite = sprite;
+    this.pokemonList = pokemonList;
+    this.itens = itens;
+
+ }
