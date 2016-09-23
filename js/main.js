8ee34f6743119503d79fb83085848e491d36a4d2
Gabriel <jcfgabriel@gmail.com>
2016-09-23 00:43:40 -0300

    Move class attributes

--- js/main.js
+++ js/main.js
@@ -2,18 +2,18 @@
  * Created by miguel on 17/09/16.
  */
 function main1() {
-    img3 = document.getElementById("img1");
-    //img3 = new Image();
-    //img3.src = "../img/oak.png";
+
+    var img1 = new Image();
+    img1.src = 'img/oak.png';
+    var img2 = new Image();
+    img2.src = 'img/choose.jpg';
     var canvas = document.getElementById("main_canvas");
     var ctx = canvas.getContext("2d");
 
-    img3.onload = function(){
-        ctx.drawImage(img3, 0, 0);
+    img1.onload = function(){
+        ctx.drawImage(img1, 0, 0);
     }
-
-    //document.getElementById("foca").innerHTML = img3.src;
-
-    //ctx.fillStyle="#FF0000";
-    //ctx.fillRect(0,0,100,100);
+    img2.onload = function(){
+        ctx.drawImage(img2, 200,0, 300, 200);
+    }
 }
