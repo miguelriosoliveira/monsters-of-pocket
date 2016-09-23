575a227e4eb7732b5ebd41cbf5e7ee8002d8a779
Miguel Rios <miguelriosoliveira@gmail.com>
2016-09-23 11:37:19 -0300

    WIP: Adicionando logger no lado esquerdo da tela.

--- js/main.js
+++ js/main.js
@@ -2,35 +2,37 @@
  * Created by miguel on 17/09/16.
  */
 requirejs(["NPC", "Player", "Pokemon"], function (_NPC, _Player, _Pokemon) {
-	//This function is called when the scripts are loaded.
+    //This function is called when the scripts are loaded.
 
-	var canvas = document.getElementById('game-screen');
-	var ctx = canvas.getContext("2d");
+    // canvas and logger setup
+    var canvas = document.getElementById('game-screen');
+    canvas.setAttribute('width', ((2/3)*screen.availWidth).toString());
+    canvas.setAttribute('height', ((2/3)*screen.availHeight).toString());
+    var ctx = canvas.getContext("2d");
+    var logger = document.getElementById('left-logger');
+    logger.style.maxWidth = ((1/3)*screen.availWidth).toString()+'px';
+    logger.style.maxHeight = screen.availHeight.toString()+'px';
 
-	// criar player
-	var player = new Player('Red', 'M');
+    // criar player
+    var player = new Player('Red', 'M');
 
-	// criar prof carvalho e rival
-	var bulbasaur = new Pokemon('bulbasaur', 1);
-	var squirtle = new Pokemon('squirtle', 1);
-	var charmander = new Pokemon('charmander', 1);
-	var starters = [bulbasaur, squirtle, charmander];
-	var items = ['pokedex'];
-	var oak = new NPC('Professor Oak', '', starters, items);
-	var rival = new NPC('Blue', '', [], []);
+    // criar prof carvalho e rival
+    var bulbasaur = new Pokemon('bulbasaur', 1);
+    var squirtle = new Pokemon('squirtle', 1);
+    var charmander = new Pokemon('charmander', 1);
+    var starters = [bulbasaur, squirtle, charmander];
+    var items = ['pokedex'];
+    var oak = new NPC('Professor Oak', '', starters, items);
+    var rival = new NPC('Blue', '', [], []);
 
-	console.log('Red choose Charmander!');
-	console.log('So Blue will choose Squirtle!');
+    console.log('Red choose Charmander!');
+    console.log('So Blue will choose Squirtle!');
 
     var img1 = new Image();
     img1.src = 'img/oak.png';
     var img2 = new Image();
     img2.src = 'img/choose.jpg';
 
-    img1.onload = function(){
-        ctx.drawImage(img1, 0, 0);
-    }
-    img2.onload = function(){
-        ctx.drawImage(img2, 200,0, 300, 200);
-    }
+    // ctx.drawImage(img1, 0, 0);
+    // ctx.drawImage(img2, 200, 0, 300, 200);
 });
