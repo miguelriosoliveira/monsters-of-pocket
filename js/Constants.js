b0ed5eff1bde2249ef571f4a448f2b53c47b189e
Miguel Rios <miguelriosoliveira@gmail.com>
2016-11-08 10:22:15 -0200

    Game constants

---
+++ js/Constants.js
@@ -0,0 +1,13 @@
+/**
+ * Created by miguel on 08/11/16.
+ */
+
+/**
+ * Basic constants of the game
+ * @param canvas
+ * @constructor
+ */
+var Constants = function (canvas) {
+    var SCREEN_HEIGHT = canvas.getAttribute('height');
+    var SCREEN_WIDTH = canvas.getAttribute('width');
+};
--- js/main.js
+++ js/main.js
@@ -8,53 +8,73 @@
 var canvas = document.getElementById('game-screen');
 var ctx = canvas.getContext("2d");
 
+/* redirect messages to the logger */
+function redirectLog(loggerTag) {
+	console.log = function (message) {
+		loggerTag.innerHTML += message + '\n';
+		loggerTag.scrollTop = loggerTag.scrollHeight;
+	};
+}
+
+/* initial settings */
 function init() {
 	canvas.setAttribute('width', ((3 / 5) * screen.availWidth).toString());
 	canvas.setAttribute('height', ((3 / 5) * screen.availHeight).toString());
 
 	var logger = document.getElementById('left-logger');
 	logger.style.width = ((1 / 3) * screen.availWidth).toString() + 'px';
-	logger.style.height = screen.availHeight.toString() + 'px';
+	logger.style.height = canvas.getAttribute('height') + 'px';
 
-	// capturando logs do console.log
 	redirectLog(logger);
 }
 
-function redirectLog(loggerTag) {
-	console.log = function (message) {
-		// redirect message to the logger
-		loggerTag.innerHTML += message + '\n';
-		loggerTag.scrollTop = loggerTag.scrollHeight;
-	};
-}
-
-requirejs(["NPC", "Player", "Pokemon"], function (_NPC, _Player, _Pokemon) {
+requirejs(["Constants", "NPC", "Player", "Pokemon"], function (_Constants, _NPC, _Player, _Pokemon) {
 	init();
+	var Constants = new Constants(canvas);
 
-	// criar player
+	// the player
 	var player = new Player('Red', 'M');
 
 	// criar prof carvalho e rival
-	var bulbasaur = new Pokemon('bulbasaur', 1);
-	var squirtle = new Pokemon('squirtle', 1);
-	var charmander = new Pokemon('charmander', 1);
+	/*var bulbasaur	= new Pokemon('bulbasaur', 1);
+	var squirtle	= new Pokemon('squirtle', 1);
+	var charmander	= new Pokemon('charmander', 1);
 	var starters = [bulbasaur, squirtle, charmander];
 	var items = ['pokedex'];
 	var oak = new NPC('Professor Oak', '', starters, items);
-	var rival = new NPC('Blue', '', [], []);
+	var rival = new NPC('Blue', '', [], []);*/
 
 	//sprite do player na tela
-	var img_player = new Image();
+	/*var img_player = new Image();
 
 	img_player.src = player.sprite;
 	img_player.src += 'Player_Back_0.png';
 
 	img_player.onload = function () {
-		player.position = new Position(canvas.getAttribute('width') / 2, canvas.getAttribute('height') / 2);
+		player.position = new Position(Constants.SCREEN_WIDTH / 2, Constants.SCREEN_HEIGHT / 2);
 		ctx.drawImage(img_player, player.position.x, player.position.y);
-	};
+	};*/
 
-	//animaçao de andar
+	/* clear screen */
+	function clear() {
+		ctx.clearRect(0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
+	}
+
+	/* draw all elements */
+	function draw() {
+		clear();
+		ctx.fillStyle = "white";
+		ctx.strokeStyle = "black";
+		// rect(0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
+		ctx.fillStyle = "purple";
+		// circle(x, y, 10);
+		ctx.drawImage(img_player, player.position.x, player.position.y);
+	}
+
+	/* main loop */
+	setInterval(draw, 10);
+
+	/* keyboard listener */
 	function keyDown(evt) {
 		var delta = 10;
 		switch (evt.keyCode) {
@@ -62,10 +82,10 @@ requirejs(["NPC", "Player", "Pokemon"], function (_NPC, _Player, _Pokemon) {
 				player.moveUp(delta, 0);
 				break;
 			case 39:  /* Right arrow was pressed */
-				player.moveRight(delta, canvas.getAttribute('width'));
+				player.moveRight(delta, Constants.SCREEN_WIDTH);
 				break;
 			case 40:  /* Down arrow was pressed */
-				player.moveDown(delta, canvas.getAttribute('height'));
+				player.moveDown(delta, Constants.SCREEN_HEIGHT);
 				break;
 			case 37:  /* Left arrow was pressed */
 				player.moveLeft(delta, 0);
@@ -73,40 +93,5 @@ requirejs(["NPC", "Player", "Pokemon"], function (_NPC, _Player, _Pokemon) {
 		}
 	}
 
-	function clear() {
-		ctx.clearRect(0, 0, Number(canvas.getAttribute('width')), Number(canvas.getAttribute('height')));
-	}
-
-	function draw() {
-		clear();
-		ctx.fillStyle = "white";
-		ctx.strokeStyle = "black";
-		// rect(0, 0, canvas.getAttribute('width'), canvas.getAttribute('height'));
-		ctx.fillStyle = "purple";
-		// circle(x, y, 10);
-		ctx.drawImage(img_player, player.position.x, player.position.y);
-	}
-
-	setInterval(draw, 10);
-
-	init();
 	window.addEventListener('keydown', keyDown, true);
-
-	/*
-	 arrow keys are only triggered by onkeydown, not onkeypress
-	 keycodes are:
-	 left = 37
-	 up = 38
-	 right = 39
-	 down = 40
-	 */
-
-	//example of how to draw on canvas
-	//var img2 = new Image();
-	//img2.src = 'img/choose.jpg';
-	//var canvas = document.getElementById("game-screen");
-	//var ctx = canvas.getContext("2d");
-	//img2.onload = function (){
-	//	ctx.drawImage(img2, 0, 0);
-	//}
 });
